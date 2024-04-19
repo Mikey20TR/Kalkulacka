4efcb9081f0e1564db5a192502d814dbf9dd320c
Mikey20TR <Mikejbc@email.cz>
2024-04-19 11:34:18 +0200

    Commit kalkulacky

---
+++ Alpha/Kalkulacka.cs
@@ -0,0 +1,38 @@
+﻿namespace Kalkulacka
+{
+
+    public class Kalkulacka
+    {
+        public double AktualniVysledek = 0;
+        public double Pricti(double cisloUzivatele)
+        {
+            return AktualniVysledek = AktualniVysledek + cisloUzivatele;
+        }
+        public double Odecti(double cisloUzivatele)
+        {
+            return AktualniVysledek = AktualniVysledek - cisloUzivatele;
+        }
+        public double Vynasob(double cisloUzivatele)
+        {
+            return AktualniVysledek = AktualniVysledek * cisloUzivatele;
+        }
+        public double Vydel(double cisloUzivatele)
+        {
+            return AktualniVysledek = AktualniVysledek / cisloUzivatele;
+        }
+        public double Umocni(double cisloUzivatele)
+        {
+            double mocnenec = AktualniVysledek;
+            AktualniVysledek = 1;
+            for (int i = 1; i <= cisloUzivatele; i++)
+            {
+                AktualniVysledek = AktualniVysledek * mocnenec;
+            }
+            return AktualniVysledek;
+        }
+        public double VratAktualniVysledek()
+        {
+            return AktualniVysledek;
+        }
+    }
+}
---
+++ Alpha/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Kalkulacka
+{
+    internal class Program
+    {
+        public static double NactiDesetinneCisloZKonzole()
+        {
+            string cisloText = Console.ReadLine();
+            bool jeCislo = double.TryParse(cisloText, out double cislo);
+            while (jeCislo == false)
+            {
+                Console.WriteLine("Neplatné zadání, zkus to znovu:");
+                cisloText = Console.ReadLine();
+                jeCislo = double.TryParse(cisloText, out cislo);
+            }
+            return cislo;
+        }
+        public static string NactiOperatorZKonzole()
+        {
+            string operat = Console.ReadLine();
+            while (operat != "+" && operat != "-" && operat != "*" && operat != "/" && operat != "^" && operat != "X")
+            {
+                Console.WriteLine("Zadal jsi špatně operátor, zkus to znovu:");
+                operat = Console.ReadLine();
+            }
+            return operat;
+        }
+        static void Main(string[] args)
+        {
+            Kalkulacka kacka = new Kalkulacka { };
+
+            Console.WriteLine("Ahoj, uživateli");
+            Console.WriteLine("Zadej první číslo:");
+            kacka.AktualniVysledek = NactiDesetinneCisloZKonzole();
+            while (true)
+            {
+                Console.WriteLine("Zadej operátor:");
+                Console.WriteLine("Pro ukončení kalkulačky zadej X");
+                string operat = NactiOperatorZKonzole();
+                if (operat == "X")
+                {
+                    return;
+                }
+                Console.WriteLine("Zadej druhé číslo:");
+                double cisloUzivatele = NactiDesetinneCisloZKonzole();
+                switch (operat)
+                {
+                    case "+":
+                        kacka.Pricti(cisloUzivatele);
+                        break;
+                    case "-":
+                        kacka.Odecti(cisloUzivatele);
+                        break;
+                    case "*":
+                        kacka.Vynasob(cisloUzivatele);
+                        break;
+                    case "/":
+                        kacka.Vydel(cisloUzivatele);
+                        break;
+                    case "^":
+                        kacka.Umocni(cisloUzivatele);
+                        break;
+                }
+                Console.WriteLine($"Výsledek: {kacka.VratAktualniVysledek()}");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
